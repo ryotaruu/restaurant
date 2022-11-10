fb23de67c2d4b508fede1101b9fe3d2260fb53ac
Senkiysma <nguyenminhduc.sj@gmail.com>
2022-11-10 17:28:13 +0700

    sen

---
+++ resources/views/NotAdmin.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Error</title>
+</head>
+<body>
+	<div class="content">
+		<h3>You not Admin, you no power use this page!</h3>
+	</div>
+</body>
+</html>
