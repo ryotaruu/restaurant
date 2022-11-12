5bd93b45b84eba2b6c3096d2fdd363ae15378b00
Senkiysma <nguyenminhduc.sj@gmail.com>
2022-11-12 08:49:26 +0700

    senpushcode

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\ReservationController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,5 +16,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 Route::middleware(['auth','admin'])->name('admin.')->prefix('admin')->group(function(){
    Route::get('/',[AdminController::class, 'index'])->name('index');
+   Route::resource('/categories',CategoryController::class);
+   Route::resource('/menus',MenuController::class);
+   Route::resource('/tables',TableController::class);
+   Route::resource('/reservation',ReservationController::class);
 });
 require __DIR__.'/auth.php';
